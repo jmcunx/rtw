fce61881529d29550158ede3c19b06bb0c497a7d
John McCue <jmccue@jmcunx.com>
2021-11-28 11:42:57 -0500

    adjusted

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
-#
+#===========================================================================
 # Build script to use a generic
 # Makefile for install
 #
